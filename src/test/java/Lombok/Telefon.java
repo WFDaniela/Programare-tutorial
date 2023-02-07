d3948de0155e0ec6a5308cb77570546dbcb42818
WFDaniela <daniela.maria.solomon@gmail.com>
2023-02-07 19:08:50 +0200

    Add lombok

---
+++ src/test/java/Lombok/Telefon.java
@@ -0,0 +1,20 @@
+package Lombok;
+
+import lombok.*;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Telefon {
+
+    private String Brand;
+    private String Model;
+    private Integer AnFabricatie;
+    private String Material;
+    private String Camera;
+
+
+  }
